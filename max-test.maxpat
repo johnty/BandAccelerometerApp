1ff8f50bb305a49a8329c4b84d18ff5e38378c3b
johnty <johntywang@gmail.com>
2016-03-02 15:38:54 -0500

    initial commit

---
+++ max-test.maxpat
@@ -0,0 +1,226 @@
+{
+	"patcher" : 	{
+		"fileversion" : 1,
+		"appversion" : 		{
+			"major" : 6,
+			"minor" : 1,
+			"revision" : 10,
+			"architecture" : "x86"
+		}
+,
+		"rect" : [ 597.0, 219.0, 640.0, 480.0 ],
+		"bglocked" : 0,
+		"openinpresentation" : 0,
+		"default_fontsize" : 12.0,
+		"default_fontface" : 0,
+		"default_fontname" : "Arial",
+		"gridonopen" : 0,
+		"gridsize" : [ 15.0, 15.0 ],
+		"gridsnaponopen" : 0,
+		"statusbarvisible" : 2,
+		"toolbarvisible" : 1,
+		"boxanimatetime" : 200,
+		"imprint" : 0,
+		"enablehscroll" : 1,
+		"enablevscroll" : 1,
+		"devicewidth" : 0.0,
+		"description" : "",
+		"digest" : "",
+		"tags" : "",
+		"boxes" : [ 			{
+				"box" : 				{
+					"fontname" : "Arial",
+					"fontsize" : 16.0,
+					"id" : "obj-16",
+					"maxclass" : "number",
+					"numinlets" : 1,
+					"numoutlets" : 2,
+					"outlettype" : [ "int", "bang" ],
+					"parameter_enable" : 0,
+					"patching_rect" : [ 340.0, 231.0, 55.0, 24.0 ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"fontname" : "Arial",
+					"fontsize" : 12.0,
+					"id" : "obj-14",
+					"maxclass" : "newobj",
+					"numinlets" : 2,
+					"numoutlets" : 2,
+					"outlettype" : [ "", "" ],
+					"patching_rect" : [ 340.0, 193.0, 97.0, 20.0 ],
+					"text" : "route /band/light"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"fontname" : "Arial",
+					"fontsize" : 16.0,
+					"id" : "obj-12",
+					"maxclass" : "number",
+					"numinlets" : 1,
+					"numoutlets" : 2,
+					"outlettype" : [ "int", "bang" ],
+					"parameter_enable" : 0,
+					"patching_rect" : [ 511.0, 231.0, 55.0, 24.0 ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"fontname" : "Arial",
+					"fontsize" : 12.0,
+					"id" : "obj-10",
+					"maxclass" : "newobj",
+					"numinlets" : 2,
+					"numoutlets" : 2,
+					"outlettype" : [ "", "" ],
+					"patching_rect" : [ 511.0, 193.0, 123.0, 20.0 ],
+					"text" : "route /band/heartrate"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"fontname" : "Arial",
+					"fontsize" : 16.0,
+					"id" : "obj-9",
+					"maxclass" : "flonum",
+					"numinlets" : 1,
+					"numoutlets" : 2,
+					"outlettype" : [ "float", "bang" ],
+					"parameter_enable" : 0,
+					"patching_rect" : [ 199.0, 231.0, 64.0, 24.0 ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"fontname" : "Arial",
+					"fontsize" : 12.0,
+					"id" : "obj-7",
+					"maxclass" : "newobj",
+					"numinlets" : 2,
+					"numoutlets" : 2,
+					"outlettype" : [ "", "" ],
+					"patching_rect" : [ 199.0, 193.0, 123.0, 20.0 ],
+					"text" : "route /band/skintemp"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"candicane2" : [ 1.0, 0.112426, 0.0, 1.0 ],
+					"id" : "obj-3",
+					"maxclass" : "multislider",
+					"numinlets" : 1,
+					"numoutlets" : 2,
+					"outlettype" : [ "", "" ],
+					"parameter_enable" : 0,
+					"patching_rect" : [ 20.0, 231.0, 167.0, 139.0 ],
+					"setstyle" : 2,
+					"size" : 3,
+					"thickness" : 4
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"fontname" : "Arial",
+					"fontsize" : 12.0,
+					"id" : "obj-2",
+					"maxclass" : "newobj",
+					"numinlets" : 2,
+					"numoutlets" : 2,
+					"outlettype" : [ "", "" ],
+					"patching_rect" : [ 20.0, 193.0, 103.0, 20.0 ],
+					"text" : "route /band/accel"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"fontname" : "Arial",
+					"fontsize" : 12.0,
+					"id" : "obj-1",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 272.0, 138.0, 99.0, 20.0 ],
+					"text" : "udpreceive 7000"
+				}
+
+			}
+ ],
+		"lines" : [ 			{
+				"patchline" : 				{
+					"destination" : [ "obj-10", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-1", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-2", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-1", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-7", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-1", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-12", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-10", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-16", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-14", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-3", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-2", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-9", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-7", 0 ]
+				}
+
+			}
+ ],
+		"dependency_cache" : [  ]
+	}
+
+}
---
+++ src/com/microsoft/band/sdk/sampleapp/accelerometer/BandAccelerometerAppActivity.java
@@ -0,0 +1,272 @@
+//Copyright (c) Microsoft Corporation All rights reserved.  
+// 
+//MIT License: 
+// 
+//Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+//documentation files (the  "Software"), to deal in the Software without restriction, including without limitation
+//the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+//to permit persons to whom the Software is furnished to do so, subject to the following conditions: 
+// 
+//The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+//the Software. 
+// 
+//THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+//TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+//THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+//CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+//IN THE SOFTWARE.
+package com.microsoft.band.sdk.sampleapp.accelerometer;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.microsoft.band.BandClient;
+import com.microsoft.band.BandClientManager;
+import com.microsoft.band.BandException;
+import com.microsoft.band.BandIOException;
+import com.microsoft.band.BandInfo;
+import com.microsoft.band.ConnectionState;
+import com.microsoft.band.sensors.BandAccelerometerEvent;
+import com.microsoft.band.sensors.BandAccelerometerEventListener;
+import com.microsoft.band.sensors.BandAmbientLightEventListener;
+import com.microsoft.band.sensors.BandHeartRateEvent;
+import com.microsoft.band.sensors.BandHeartRateEventListener;
+import com.microsoft.band.sensors.BandSkinTemperatureEvent;
+import com.microsoft.band.sensors.BandSkinTemperatureEventListener;
+import com.microsoft.band.UserConsent;
+import com.microsoft.band.sensors.BandHeartRateEvent;
+import com.microsoft.band.sensors.BandHeartRateEventListener;
+import com.microsoft.band.sensors.HeartRateConsentListener;
+import com.microsoft.band.sensors.BandAmbientLightEvent;
+import com.microsoft.band.sensors.SampleRate;
+
+import com.illposed.osc.*;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class BandAccelerometerAppActivity extends Activity {
+
+	private BandClient client = null;
+	private Button btnStart;
+	private TextView txtStatus;
+
+	private static OSCPortOut myOSCSender;
+
+	//we need to do networking stuff in an AsyncTask to avoid NetworkOnMainThreadException
+	private class sendOSCTask extends AsyncTask<OSCMessage, Integer, Long> {
+		protected Long doInBackground(OSCMessage... msgs) {
+			try {
+				myOSCSender.send(msgs[0]);
+			}
+			catch (IOException e){
+
+			}
+
+
+			return 0L;
+		}
+	};
+
+	private BandAccelerometerEventListener mAccelerometerEventListener = new BandAccelerometerEventListener() {
+        @Override
+        public void onBandAccelerometerChanged(final BandAccelerometerEvent event) {
+            if (event != null) {
+            	appendToUI(String.format(" X = %.3f \n Y = %.3f\n Z = %.3f", event.getAccelerationX(),
+            			event.getAccelerationY(), event.getAccelerationZ()));
+
+				float x = event.getAccelerationX();
+				float y = event.getAccelerationY();
+				float z = event.getAccelerationZ();
+				OSCMessage msg = new OSCMessage("/band/accel");
+				msg.addArgument(x);
+				msg.addArgument(y);
+				msg.addArgument(z);
+				new sendOSCTask().execute(msg);
+            }
+        }
+    };
+
+	private BandSkinTemperatureEventListener mSkinTemperatureEventListener = new BandSkinTemperatureEventListener() {
+		@Override
+		public void onBandSkinTemperatureChanged(BandSkinTemperatureEvent bandSkinTemperatureEvent) {
+			if (bandSkinTemperatureEvent != null) {
+				float skinTemp = bandSkinTemperatureEvent.getTemperature();
+				OSCMessage msg = new OSCMessage("/band/skintemp");
+				msg.addArgument(skinTemp);
+				new sendOSCTask().execute(msg);
+
+			}
+		}
+	};
+
+    //note: needs to set up consent for this to work!!!
+	private BandHeartRateEventListener mHeartRateEventListener = new BandHeartRateEventListener() {
+		@Override
+		public void onBandHeartRateChanged(BandHeartRateEvent bandHeartRateEvent) {
+			if (bandHeartRateEvent != null) {
+				int hr = bandHeartRateEvent.getHeartRate();
+				OSCMessage msg = new OSCMessage("/band/heartrate");
+				msg.addArgument(hr);
+				new sendOSCTask().execute(msg);
+			}
+		}
+	};
+
+    private BandAmbientLightEventListener mAmbientLightEventListener = new BandAmbientLightEventListener() {
+        @Override
+        public void onBandAmbientLightChanged(BandAmbientLightEvent bandAmbientLightEvent) {
+            if (bandAmbientLightEvent != null) {
+                int light = bandAmbientLightEvent.getBrightness();
+                OSCMessage msg = new OSCMessage("/band/light");
+                msg.addArgument(light);
+                new sendOSCTask().execute(msg);
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+		try {
+			myOSCSender = new OSCPortOut(InetAddress.getByName("192.168.0.100"), 7000);
+			OSCMessage msg = new OSCMessage("/hello/world");
+			msg.addArgument(3);
+			msg.addArgument(4);
+
+
+			new sendOSCTask().execute(msg);
+		}
+		catch (UnknownHostException e) {
+			Log.e("ERR", "uknown");
+
+		}
+		catch (SocketException e) {
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		txtStatus = (TextView) findViewById(R.id.txtStatus);
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				txtStatus.setText("");
+				new SensorSubscriptionTask().execute();
+			}
+		});
+    }
+
+    @Override
+	protected void onResume() {
+		super.onResume();
+		txtStatus.setText("");
+	}
+
+    @Override
+	protected void onPause() {
+		super.onPause();
+		if (client != null) {
+			try {
+				//client.getSensorManager().unregisterAccelerometerEventListener(mAccelerometerEventListener);
+				//client.getSensorManager().unregisterSkinTemperatureEventListener(mSkinTemperatureEventListener);
+                client.getSensorManager().unregisterAllListeners();
+			} catch (BandIOException e) {
+				appendToUI(e.getMessage());
+			}
+		}
+	}
+
+	private class SensorSubscriptionTask extends AsyncTask<Void, Void, Void> {
+		@Override
+		protected Void doInBackground(Void... params) {
+			try {
+                if (getConnectedBandClient()) {
+					appendToUI("Band is connected.\n");
+					client.getSensorManager().registerAccelerometerEventListener(mAccelerometerEventListener, SampleRate.MS32);
+					client.getSensorManager().registerSkinTemperatureEventListener(mSkinTemperatureEventListener);
+                    client.getSensorManager().registerAmbientLightEventListener(mAmbientLightEventListener);
+                    if (client.getSensorManager().getCurrentHeartRateConsent() == UserConsent.GRANTED) {
+                        client.getSensorManager().registerHeartRateEventListener(mHeartRateEventListener);
+                    }
+                    else {
+                        Log.v("VERBOSE", "No HR Consent!!");
+                    }
+
+				} else {
+					appendToUI("Band isn't connected. Please make sure bluetooth is on and the band is in range.\n");
+				}
+			} catch (BandException e) {
+				String exceptionMessage="";
+				switch (e.getErrorType()) {
+				case UNSUPPORTED_SDK_VERSION_ERROR:
+					exceptionMessage = "Microsoft Health BandService doesn't support your SDK Version. Please update to latest SDK.\n";
+					break;
+				case SERVICE_ERROR:
+					exceptionMessage = "Microsoft Health BandService is not available. Please make sure Microsoft Health is installed and that you have the correct permissions.\n";
+					break;
+				default:
+					exceptionMessage = "Unknown error occured: " + e.getMessage() + "\n";
+					break;
+				}
+				appendToUI(exceptionMessage);
+
+			} catch (Exception e) {
+				appendToUI(e.getMessage());
+			}
+			return null;
+		}
+	}
+
+    @Override
+    protected void onDestroy() {
+        if (client != null) {
+            try {
+                client.disconnect().await();
+            } catch (InterruptedException e) {
+                // Do nothing as this is happening during destroy
+            } catch (BandException e) {
+                // Do nothing as this is happening during destroy
+            }
+        }
+        super.onDestroy();
+    }
+
+	private void appendToUI(final String string) {
+		this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+            	txtStatus.setText(string);
+            }
+        });
+	}
+
+	private boolean getConnectedBandClient() throws InterruptedException, BandException {
+		if (client == null) {
+			BandInfo[] devices = BandClientManager.getInstance().getPairedBands();
+			if (devices.length == 0) {
+				appendToUI("Band isn't paired with your phone.\n");
+				return false;
+			}
+			client = BandClientManager.getInstance().create(getBaseContext(), devices[0]);
+		} else if (ConnectionState.CONNECTED == client.getConnectionState()) {
+			return true;
+		}
+
+		appendToUI("Band is connecting...\n");
+		return ConnectionState.CONNECTED == client.connect().await();
+	}
+}
+
